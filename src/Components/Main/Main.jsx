1b5d71f27a332d32002bdb4f966b14a9a98d0203
Simon <simonfasil78@gmail.com>
2023-09-25 19:31:27 +0400

    The menu section and the testimonials sections are completed. only the testimonials section is not responsive

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Main.css'
+import Testimonials from './Testimonials/Testimonials'
 function Main() {
   return (
     <main>
@@ -14,8 +15,49 @@ function Main() {
             <div className='HeroImgContainer'>
             <img className='HeroImg' src={require('../../Assets/restaurant chef B.jpg')}  alt='banner of little lemon'/>
             </div>
-           
         </section>
+
+        <section aria-labelledby='SpecialsID' className='SpecialsSection'>
+            <div className='SpecialsTitleContainer'>
+            <h1 id="SpecialsID"> This weeks specials!</h1>
+            <button type='button' className='showMenuButton'> Show full menu</button>
+            </div>
+
+            <div className='MenuContanier'>
+            <img className='foodImage' src={require('../../Assets/lemon dessert.jpg')}  alt='banner of little lemon'/>
+            <div className='foodNamePricecontainer'>
+            <h2 className='foodName'>Lemon dessert</h2>
+            <span className='foodPrice' >$12.99</span>
+            </div>
+            <p className='foodDescription'>We are a family owned Mediterranean restaurant,
+            Focused on traditional recipes served with a modern twist</p>
+            <button type='button' className='orderButton'>Order a delivery </button>
+            </div>
+
+            <div className='MenuContanier'>
+            <img className='foodImage' src={require('../../Assets/greek salad.jpg')}  alt='banner of little lemon'/>
+            <div className='foodNamePricecontainer'>
+            <h2 className='foodName'>Greek Salad</h2>
+            <span className='foodPrice' >$12.99</span>
+            </div>
+            <p className='foodDescription'>We are a family owned Mediterranean restaurant,
+            Focused on traditional recipes served with a modern twist</p>
+            <button type='button' className='orderButton'>Order a delivery </button>
+            </div>
+
+            <div className='MenuContanier'>
+            <img className='foodImage' src={require('../../Assets/fddine180706_photos.jpg')}  alt='banner of little lemon'/>
+            <div className='foodNamePricecontainer'>
+            <h2 className='foodName'>Crab Corn</h2>
+            <span className='foodPrice' >$12.99</span>
+            </div>
+            <p className='foodDescription'>We are a family owned Mediterranean restaurant,
+            Focused on traditional recipes served with a modern twist</p>
+            <button type='button' className='orderButton'>Order a delivery </button>
+            </div>
+        </section>
+
+        <Testimonials/>
     </main>
   )
 }
